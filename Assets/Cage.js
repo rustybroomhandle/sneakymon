72ce20784079f3223cbab50080cd430e13d73239
jaco <jaco.gerber@gmail.com>
2015-08-24 17:34:46 +0200

    Monday 2

--- Assets/Cage.js
+++ Assets/Cage.js
@@ -1,9 +1,23 @@
 ﻿#pragma strict
 
-function Start () {
+var freemonster : String;
+var freemonstername : String;
+
+function OpenCage () {
   GetComponent.<Animation>().Play("Take 001");
-}
+  yield WaitForSeconds(1);
+  GameObject.Find("monsterfree").transform.localPosition.y = 0;
 
-function Update () {
+  GameObject.Find("freetxt").GetComponent(TextMesh).text = freemonstername + "\nis free !";
 
+  GameObject.Find(freemonster).transform.SetParent(GameObject.Find("monsterfree").transform);
+  GameObject.Find(freemonster).transform.position=GameObject.Find("monsterfree").transform.position;
+  GameObject.Find(freemonster).transform.rotation=GameObject.Find("monsterfree").transform.rotation;
+
+  yield WaitForSeconds(3);
+  Destroy(GameObject.Find(freemonster));
+
+  GameObject.Find("monsterfree").transform.localPosition.y = 999;
+
+  freemonster = "free";
 }
--- Assets/Char.js
+++ Assets/Char.js
@@ -5,16 +5,29 @@ private var moveRight : boolean = false;
 private var moveLeft : boolean = false;
 private var moveForward : boolean = false;
 private var moveBackward : boolean = false;
-private var moveSpeed : float =  10.0;
+private var moveSpeed : float =  6.0;
+
+private var beefeeanim : Animation;
+
+
 var keys : int = 0;
 
 function Start () {
+  beefeeanim = GameObject.Find("beefee").GetComponent.<Animation>();
+}
 
+function BRun() {
+  beefeeanim.Play("Take 001");
+}
+
+function BStop() {
+  beefeeanim.Stop();
 }
 
 function Update() {
   if (Input.GetKeyDown("d") || Input.GetKeyDown(KeyCode.RightArrow)) {
     	moveRight=true;
+      BRun();
 	}
 	if (Input.GetKeyUp("d") || Input.GetKeyUp(KeyCode.RightArrow)) {
 	    moveRight=false;
@@ -22,6 +35,7 @@ function Update() {
 
 	if (Input.GetKeyDown("a") || Input.GetKeyDown(KeyCode.LeftArrow)) {
 	    moveLeft=true;
+      BRun();
 	}
 	if (Input.GetKeyUp("a") || Input.GetKeyUp(KeyCode.LeftArrow)) {
 	    moveLeft=false;
@@ -29,6 +43,7 @@ function Update() {
 
 	if (Input.GetKeyDown("w") || Input.GetKeyDown(KeyCode.UpArrow)) {
 	    moveForward=true;
+      BRun();
 	}
 	if (Input.GetKeyUp("w") || Input.GetKeyUp(KeyCode.UpArrow)) {
 	    moveForward=false;
@@ -36,34 +51,45 @@ function Update() {
 
 	if (Input.GetKeyDown("s") || Input.GetKeyDown(KeyCode.DownArrow)) {
 	    moveBackward=true;
+      BRun();
 	}
 	if (Input.GetKeyUp("s") || Input.GetKeyUp(KeyCode.DownArrow)) {
 	    moveBackward=false;
 	}
 
+  if (!moveLeft && !moveRight && !moveForward && !moveBackward) {
+      BStop();
+  }
+
   if (Input.GetKeyDown("e")) {
-      if (touching && Vector3.Distance(touching.transform.position, transform.position) < 3) {
-        print("searching");
+      if (touching && Vector3.Distance(touching.transform.position, transform.position) < 4 && touching.tag=="container") {
         searching = true;
+      }
+      if (touching && Vector3.Distance(touching.transform.position, transform.position) < 4 && touching.tag=="cage" && keys > 0) {
+        freeing = true;
       }
   }
   if (Input.GetKeyUp("e")) {
-    print("not searching");
     searchcount = 0;
     searching = false;
+    freeing = false;
   }
 }
 
 var searching : boolean = false;
+var freeing : boolean = false;
 var searchcount : float = 0;
+var freecount : int = 10;
 
 function FixedUpdate() {
 
   GameObject.Find("keystxt").GetComponent(TextMesh).text = keys.ToString();
+  GameObject.Find("freecounttxt").GetComponent(TextMesh).text = freecount.ToString();
 
-  if (touching) {
+  if (touching && touching.GetComponent(Container)) {
     var searched = touching.GetComponent(Container).searched;
     var containerkeys = touching.GetComponent(Container).key;
+    var tag : String = touching.tag;
   }
   if (searching && searched == false) {
     if (searchcount < 100) {
@@ -78,6 +104,18 @@ function FixedUpdate() {
       touching.transform.Find("srched").transform.localPosition.y = 0;
       touching.GetComponent(Container).searched = true;
       searching = false;
+    }
+  }
+  if (freeing && touching.GetComponent(Cage).freemonster != "free") {
+    if (searchcount < 100) {
+      touching.transform.Find("srch").transform.localPosition.y = 3;
+      searchcount+=0.5;
+    } else {
+      touching.transform.Find("srch").transform.localPosition.y = 1000;
+      freeing = false;
+      keys = keys - 1;
+      freecount = freecount - 1;
+      touching.SendMessage("OpenCage");
     }
   }
   if (touching) {
@@ -115,7 +153,7 @@ function FixedUpdate() {
 var touching : GameObject;
 
 function OnCollisionEnter(collision: Collision) {
-  if (collision.gameObject.tag == "container") {
+  if (collision.gameObject.tag == "container" || collision.gameObject.tag == "cage") {
     touching = collision.gameObject;
   }
 }
